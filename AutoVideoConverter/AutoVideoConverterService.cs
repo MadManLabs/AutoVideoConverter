d042b26dc076d2a2a21b0ba93537fa7cc90deba5
Connor Wilson <Connorwilson1@googlemail.com>
2014-10-16 15:10:58 +0100

    Initial Commit
    Initial Commit

---
+++ AutoVideoConverter/AutoVideoConverterService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoVideoConverter
+{
+    public partial class AutoVideoConverterService : ServiceBase
+    {
+        public AutoVideoConverterService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            FileSystemWatcher fsw  = new FileSystemWatcher()
+            {
+                EnableRaisingEvents = true
+            };
+            
+            
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
