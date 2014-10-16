ed200efaaaab9ce657576d62aa006d4a5338491a
Connor Wilson <Connorwilson1@googlemail.com>
2014-10-16 15:35:50 +0100

    Added Directory Builder
    Program now creates a directory to put the converted file into after
    conversion

--- AutoVideoConverter/AutoVideoConverterService.cs
+++ AutoVideoConverter/AutoVideoConverterService.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.ServiceProcess;
 
 
@@ -31,25 +33,36 @@ namespace AutoVideoConverter
 
         private static Queue<string> ProcessQueue = new Queue<string>();
 
-        private static bool ProgramIsBusy;
+        public static bool ProgramIsBusy;
 
         private static void ConvertVideoFromQueue(string inputPath)
         {
             //Pick up file paths of files that are created in WatchingPath directory then pass them into converter.
             var fileConverter = new NReco.VideoConverter.FFMpegConverter();
-            fileConverter.ConvertMedia(inputPath, ConfigurationSettings.AppSettings["OutputPath"], "mp4");
-            
+            //Parse inputPath and Create a directory in the OutputPath for the file.
+            var fileName = inputPath.Split('\\').Last();
+
+            var newDirectoryPath = string.Format(@"{0}\{1}", ConfigurationSettings.AppSettings["OutputPath"], fileName);
+            var outputDirectory = Directory.CreateDirectory(newDirectoryPath);
+
+            fileConverter.ConvertMedia(inputPath, outputDirectory.ToString(), "mp4");
+           
             ProgramIsBusy = false;
+            
         }
         private static void OnChange(object sender, FileSystemEventArgs e)
         {
-            //
            ProcessQueue.Enqueue(e.FullPath);
             while (ProgramIsBusy == false)
             {
-                var inputPath = ProcessQueue.Peek();
-                ConvertVideoFromQueue(inputPath);
-                ProgramIsBusy = true;
+                //Check for values in the ProcessQueue
+                if (ProcessQueue.Peek() != null)
+                {
+                    var inputPath = ProcessQueue.Peek();
+                    ConvertVideoFromQueue(inputPath);
+                    ProcessQueue.Dequeue();
+                    ProgramIsBusy = true;
+                }
             }
         }
 
